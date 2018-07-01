8d33e7e844f0c73a5883bbf58b4bfd05825efdcf
JocelynDev <jocelyndevcontact@gmail.com>
2018-07-01 14:07:24 +0200

    classes

--- eleve.rb
+++ eleve.rb
@@ -6,9 +6,24 @@ eleves = [
 
 class Eleve
 
+    def initialize(nom, age)
+        @nom = nom
+        @age = age
+    end
+
+    def bonjour
+        puts "Bonjour je suis #{@nom} et j'ai #{@age} ans."
+    end
+
+    def isMajeur
+        if(@age >= 18)
+            puts "#{@nom} est majeur !"
+        else
+            puts "#{nom} n'est pas majeur.."
+        end
+    end
 end
 
-eleve1 = Eleve.new
-eleve2 = Eleve.new
-puts eleve1.inspect
-puts eleve2.inspect
+eleve1 = Eleve.new("Jocelyn", 19)
+eleve1.bonjour
+eleve1.isMajeur
